fb8b34dad41dc6bf47dcb59aa0c1b7d48d701a36
Matthias Fuegger <mfuegger@lsv.fr>
2023-02-15 23:42:36 +0100

    meta

---
+++ main.py
@@ -0,0 +1,38 @@
+import pprint
+import trace as tr
+
+# ---- testing ------
+
+# circuit
+
+# c
+tr.rise(f=tr.Cr, i=['a','b'], o='y', d=2)
+tr.fall(f=tr.Cf, i=['a','b'], o='y', d=2)
+
+# inv
+tr.rise(f=tr.INVr, i=['y'], o='a', d=4)
+tr.fall(f=tr.INVf, i=['y'], o='a', d=4)
+
+# inv
+tr.rise(f=tr.INVr, i=['y'], o='b', d=5)
+tr.fall(f=tr.INVf, i=['y'], o='b', d=5)
+
+# inv
+#rise(f=INVr, i=['y'], o='z', d=1)
+#fall(f=INVf, i=['y'], o='z', d=1)
+
+# pprint.pprint(rules)
+# pprint.pprint(signals)
+
+# run it
+init = {'a': 1, 'b': 1, 'y': 0}
+events = [
+	(0, 'y', 0.5),  # add glitch
+]
+times, states = tr.trace(init, events=events, T=20)
+
+# print it
+for i in range(len(times)):
+	print()
+	print(f'time {times[i]}:')
+	pprint.pprint(states[i])
--- trace.py
+++ trace.py
@@ -10,6 +10,7 @@ INVf = lambda a: a
 rules = []
 signals = set()
 
+
 def rule(f, i, o, val, d=1):
 	global rules, signals
 	rules += [ {'f': f, 'i': i, 'o': o, 'val': val, 'd': d} ]
@@ -17,20 +18,31 @@ def rule(f, i, o, val, d=1):
 	signals.add(o)
 
 
-def rise(f, i, o, d=1):
-	rule(f=f, i=i, o=o, val=1, d=d)
-
-
-def fall(f, i, o, d=1):
-	rule(f=f, i=i, o=o, val=0, d=d)
-
-
 def eval_rule(state, rule):
 	args = [ state[s] for s in rule['i'] ]
 	return rule['f'](*args)
 
 
-def trace(init, T=20):
+def rise(f, i, o, d=1):
+	"""
+	add a rising PR
+	"""
+	rule(f=f, i=i, o=o, val=1, d=d)
+
+
+def fall(f, i, o, d=1):
+	"""
+	add a falling PR
+	"""
+	rule(f=f, i=i, o=o, val=0, d=d)
+
+
+def trace(init, events=[], T=20):
+	"""
+	init:   initial state. Dict of the form: signal -> value
+	events: list of items (time, signal, value)
+	T:      time until execution
+	"""
 	global rules, signals
 	t = 0
 	states = []
@@ -48,6 +60,17 @@ def trace(init, T=20):
 		# print()
 		# print(state)
 		# print('scheduled at time', t, scheduled)
+
+		# unstable events
+		unstable_rules = [ event[1] for event in scheduled if eval_rule(state=state, rule=event[1]) == 0 ]
+
+		# for all these: make glitch at output if the current value and the intended value do not match
+		for rule in unstable_rules:
+			# print('unstable at time', t, rule)
+			new_value = rule['val'] if ( state[ rule['o'] ] == rule['val'] ) else 0.5
+			state[ rule['o'] ] = new_value
+
+		# keep only stable events in scheduled
 		scheduled = [ event for event in scheduled if eval_rule(state=state, rule=event[1]) > 0 ]
 
 		# apply events
@@ -55,6 +78,11 @@ def trace(init, T=20):
 		for rule in rules_to_apply:
 			# print('apply at time', t, rule)
 			state[ rule['o'] ] = rule['val']
+
+		# potentially overwrite with external events
+		for event in events:
+			if event[0] == t:
+				state[ event[1] ] = event[2]
 		
 		# add state
 		times += [ t ]
@@ -62,15 +90,25 @@ def trace(init, T=20):
 
 		# keep only future events
 		scheduled = [ event for event in scheduled if event[0] > t ]
+		events = [ event for event in events if event[0] > t ]
 
 		# schedule new events
 		for rule in rules:
 			# if rule guard is true and effect not already the case in state
 			if eval_rule(state, rule) > 0 and state[ rule['o'] ] != rule['val']:
-				scheduled += [ (t + rule['d'], rule) ]
+				if eval_rule(state, rule) == 1:
+					scheduled += [ (t + rule['d'], rule) ]
+				else:
+					assert( eval_rule(state, rule) == 0.5 )
+					new_rule = copy.deepcopy(rule)
+					new_rule['val'] = 0.5
+					scheduled += [ (t + new_rule['d'], new_rule) ]
 				
-		# next time
-		next_t = min([T] + [ event[0] for event in scheduled ])
+		# next time:
+		#  T,
+		#  scheduled event,
+		#  external event
+		next_t = min([ T ] + [ event[0] for event in scheduled ] + [ event[0] for event in events ])
 		t = next_t
 
 	# filter (if no change in state -> remove it)
@@ -86,37 +124,3 @@ def trace(init, T=20):
 
 	return filtered_times, filtered_states
 
-
-
-# ---- testing ------
-
-# circuit
-
-# c
-rise(f=Cr, i=['a','b'], o='y', d=2)
-fall(f=Cf, i=['a','b'], o='y', d=2)
-
-# inv
-rise(f=INVr, i=['y'], o='a', d=4)
-fall(f=INVf, i=['y'], o='a', d=4)
-
-# inv
-rise(f=INVr, i=['y'], o='b', d=5)
-fall(f=INVf, i=['y'], o='b', d=5)
-
-# inv
-#rise(f=INVr, i=['y'], o='z', d=1)
-#fall(f=INVf, i=['y'], o='z', d=1)
-
-# pprint.pprint(rules)
-# pprint.pprint(signals)
-
-# run it
-init = {'a': 1, 'b': 1, 'y': 0, 'z': 0}
-times, states = trace(init)
-
-# print it
-for i in range(len(times)):
-	print()
-	print(f'time {times[i]}:')
-	pprint.pprint(states[i])
