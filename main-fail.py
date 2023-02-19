1ff021f6cee27844d3b7d5514b67fb60ad3e61ff
Matthias Fuegger <mfuegger@lsv.fr>
2023-02-19 21:34:23 +0100

    fix

--- main-fail.py
+++ main-fail.py
@@ -66,7 +66,7 @@ tr.fall(f=tr.Cf, i=['c2','en3'], o='c3', d=5)
 # times, states = tr.trace(init, events=events, T=20)
 
 init = {'c_in': 0, 'en1': 1, 'c1': 0, 'en2': 1, 'c2': 0, 'en3': 1, 'c3': 0,}
-glitch_t = 10
+glitch_t = 14
 events = [
     (glitch_t, 'en1', 1),  # add glitch
     (glitch_t + 0.1, 'en1', 0),  # reset glitch
