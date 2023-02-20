91c2c922b8c91ee5175484be36913d7b387afcb2
Matthias Fuegger <mfuegger@lsv.fr>
2023-02-20 19:19:23 +0100

    fixing

--- check.py
+++ check.py
@@ -1,9 +1,10 @@
 import trace as tr
 
-def check(times, states, T, signals, output_signals):
+def check(times, states, signals, output_signals):
 	susceptible_intervals = []
 	pos = 0
 	neg = 0
+	T = times[-1]
 
 	non_output_signals = [ s for s in signals if s not in output_signals ]
 	for s in non_output_signals:
@@ -14,9 +15,11 @@ def check(times, states, T, signals, output_signals):
     			(t + 0.1, s, states[i][s]),  # reset glitch
 			]
 			times_M, states_M = tr.trace(states[0], events=events, T=T, verbose=False)
+			
 			was_M = False
 			for state_M in states_M:
 				was_M = was_M or any([ state_M[s] == 0.5 for s in output_signals ])
+			
 			if was_M:
 				susceptible_intervals += [ (s, [times[i], times[i+1]]) ]
 				pos += times[i+1] - times[i]
