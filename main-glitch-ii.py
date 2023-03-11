1df372f8d42b982fc7dda0d98d4e7601e0281f0b
ragels90 <125493558+ragels90@users.noreply.github.com>
2023-03-11 18:19:10 +0100

    fix comments and delays

--- main-glitch-ii.py
+++ main-glitch-ii.py
@@ -6,30 +6,30 @@ import check
 # ---- testing ------
 
 # circuit
-# Muller Pipeline (ring)
+# Muller Pipeline (linear)
 
-# inv5
+# inv1
 tr.rise(f=tr.INVr, i=['c1'], o='c_in', d=4)
 tr.fall(f=tr.INVf, i=['c1'], o='c_in', d=4)
 
 
-# inv1
-tr.rise(f=tr.INVr, i=['c2'], o='en1', d=2)
-tr.fall(f=tr.INVf, i=['c2'], o='en1', d=2)
+# inv2
+tr.rise(f=tr.INVr, i=['c2'], o='en1', d=1)
+tr.fall(f=tr.INVf, i=['c2'], o='en1', d=1)
 
 # c1
 tr.rise(f=tr.Cr, i=['c_in','en1'], o='c1', d=5)
 tr.fall(f=tr.Cf, i=['c_in','en1'], o='c1', d=5)
 
-# inv2
-tr.rise(f=tr.INVr, i=['c3'], o='en2', d=2)
-tr.fall(f=tr.INVf, i=['c3'], o='en2', d=2)
+# inv3
+tr.rise(f=tr.INVr, i=['c3'], o='en2', d=1)
+tr.fall(f=tr.INVf, i=['c3'], o='en2', d=1)
 
 # c2
 tr.rise(f=tr.Cr, i=['c1','en2'], o='c2', d=5)
 tr.fall(f=tr.Cf, i=['c1','en2'], o='c2', d=5)
 
-# inv3
+# inv4
 tr.rise(f=tr.INVr, i=['c3'], o='en3', d=4)
 tr.fall(f=tr.INVf, i=['c3'], o='en3', d=4)
 
