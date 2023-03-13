2f8b4faf12e50736fd21d2bec23d490d09fff01e
ragels90 <125493558+ragels90@users.noreply.github.com>
2023-03-13 10:34:55 +0100

    cuts for sweepSink and sweepSource

--- main-muller3-sweepSource.py
+++ main-muller3-sweepSource.py
@@ -1,20 +1,20 @@
 import pprint
 import tracem as tr
 import plotting
-import check
+import checknew as check
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 def createCircuit(source_delay):
 	# circuit
-	# Muller Pipeline
+	# Muller Pipeline (linear)
 
-	# inv5
+	# inv1
 	tr.rise(f=tr.INVr, i=['c1'], o='c_in', d=source_delay)
 	tr.fall(f=tr.INVf, i=['c1'], o='c_in', d=source_delay)
 
-	# inv1
+	# inv2
 	tr.rise(f=tr.INVr, i=['c2'], o='en1', d=1)
 	tr.fall(f=tr.INVf, i=['c2'], o='en1', d=1)
 
@@ -22,7 +22,7 @@ def createCircuit(source_delay):
 	tr.rise(f=tr.Cr, i=['c_in','en1'], o='c1', d=5)
 	tr.fall(f=tr.Cf, i=['c_in','en1'], o='c1', d=5)
 
-	# inv2
+	# inv3
 	tr.rise(f=tr.INVr, i=['c3'], o='en2', d=1)
 	tr.fall(f=tr.INVf, i=['c3'], o='en2', d=1)
 
@@ -30,9 +30,9 @@ def createCircuit(source_delay):
 	tr.rise(f=tr.Cr, i=['c1','en2'], o='c2', d=5)
 	tr.fall(f=tr.Cf, i=['c1','en2'], o='c2', d=5)
 
-	# inv3
-	tr.rise(f=tr.INVr, i=['c3'], o='en3', d=1)
-	tr.fall(f=tr.INVf, i=['c3'], o='en3', d=1)
+	# inv4 (sink)
+	tr.rise(f=tr.INVr, i=['c3'], o='en3', d=4)
+	tr.fall(f=tr.INVf, i=['c3'], o='en3', d=4)
 
 	# c3
 	tr.rise(f=tr.Cr, i=['c2','en3'], o='c3', d=5)
@@ -40,50 +40,72 @@ def createCircuit(source_delay):
 
 
 T = 200
-sweep_values = np.linspace(0.1, 25, num=20)
-p = []
-for source_delay in tqdm(sweep_values):
-	# clear circuit
-	tr.clear()
-	
-	# create it
-	createCircuit(source_delay=source_delay)
-
-	# check it to get p
-	init = {
-		'c_in': 0,
-		'en1': 1,
-		'c1': 0,
-		'en2': 1,
-		'c2': 0,
-		'en3': 1,
-		'c3': 0,
-	}
-	events = []
-	times, states = tr.trace(init, events=events, T=T)
-
-	# cutoff
-	cutoff_min = 0
-	cutoff_max = float('Inf')
-
-	ret = check.check(
-		times=times,
-		events=events,
-		states=states,
-		signals=list(init.keys()),
-		output_signals=['c3', 'c1'],
-		cutoff_min=cutoff_min,
-		cutoff_max=cutoff_max
-	)
-	#pprint.pprint(ret)
-	p += [ ret['p'] ]
-
+sweep_values = np.linspace(0.1, 25, num=25)
+results = {}
 
 plt.figure()
+labels = []
+markers = []
+i = 0
+
+for sink_delay in range(4, 25, 6):
+
+    labels.append(f'sink delay = {sink_delay}')
+
+    results[sink_delay] = {'src_delay':[], 'p':[]}
+
+    for source_delay in tqdm(sweep_values):
+        # clear circuit
+        tr.clear()
+        
+        # create it
+        createCircuit(source_delay=source_delay)
+
+        # check it to get p
+        init = {
+                'c_in': 0,
+                'en1': 1,
+                'c1': 0,
+                'en2': 1,
+                'c2': 0,
+                'en3': 1,
+                'c3': 0,
+        }
+
+        events = []
+
+        times, states = tr.trace(init, events=events, T=T)
+
+        # cutoff
+        cutoff_min = 0
+        cutoff_max = float('Inf')
+
+        ret = check.check(
+                times=times,
+                events=events,
+                states=states,
+                signals=list(init.keys()),
+                output_signals=['c3', 'c1'],
+                cutoff_min=cutoff_min,
+                cutoff_max=cutoff_max
+        )
+        #pprint.pprint(ret)
+        results[sink_delay]['src_delay'] += [source_delay]
+        results[sink_delay]['p'] += [ret['p']]
+
+        # pprint.pprint(results)
+
+    plt.plot(results[sink_delay]['src_delay'], results[sink_delay]['p'], linestyle='-', marker='o', label=labels[i])
+    i += 1
+
+
 plt.xlabel('source delay [INV delay]')
 plt.ylabel('P(fail)')
-plt.plot(sweep_values, p, '*', color='blue')
+# plt.plot(sweep_values, p, '*', color='blue')
+plt.xlim(0, 25+0.5)
 plt.ylim(0,1)
+plt.legend(loc=3)
+
 fname = 'muller3_sweepSource.png'
 print(f'[info] saving figure: {fname}')
 plt.savefig(
@@ -105,3 +127,5 @@ plotting.plot(
 	susceptible=ret['susceptible'],
 	cutoff=[cutoff_min, cutoff_max],
 	)
+
+# plt.show()
