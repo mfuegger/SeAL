fd4d3e98ac7d7d83715baa07293162ffd903f9e0
ragels90 <125493558+ragels90@users.noreply.github.com>
2023-11-22 14:36:23 +0100

    checkbi for SAF

--- depricated/check.py
+++ depricated/check.py
@@ -48,7 +48,15 @@ def check(times, states, events, signals, output_signals, Mdelta=0.1, Textra=30,
 
 
 
-def checkSA(times, states, events, signals, output_signals, Mdelta=0.1, Textra=30, MafterGrid=0.01, cutoff_min=5, cutoff_max=float('Inf'), fault='SA0'):
+def checkSA(times, states, events, signals, output_signals,
+			Mdelta=0.1,
+			Textra=30,
+			MafterGrid=0.01,
+			cutoff_min=5, cutoff_max=float('Inf'),
+			exclude_output_signals=True,
+			fault='SA0'):
+	
+
 	susceptible_intervals = []
 	pos = 0
 	neg = 0
@@ -62,11 +70,12 @@ def checkSA(times, states, events, signals, output_signals, Mdelta=0.1, Textra=3
 	# 	SA = None
 
 	non_output_signals = [ s for s in signals if s not in output_signals ]
+
 	for s in non_output_signals:
 		# for i in range(len(injections)-1):
-		j = 0.1
-		for i in range(len(times)-1):
-			t = times[i]
+		j = 0
+		for i in range(len(times)):
+			# t = times[i]
 
 			# for k in np.arange(0, t, 0.1):
 			# 	print(k)
@@ -76,7 +85,8 @@ def checkSA(times, states, events, signals, output_signals, Mdelta=0.1, Textra=3
 			# 	print("k = ", k)
 			
 			# if states[i][s] == '1':
-			while j <= times[i]:
+			while j < times[i]:
+			# while j <= times[i]:
 				# events_check = events + [
 				# 	(j,          s, SA),           # add SA0 or SA1
 				# ]
@@ -90,24 +100,32 @@ def checkSA(times, states, events, signals, output_signals, Mdelta=0.1, Textra=3
 				
 				times_M, states_M = tr.traceSA(states[0], events=events_check, SA_sig=s, SA_time=j+MafterGrid, T=T, verbose=False)
 				
+				# Did X appear at any output signal at any of the circuit's states?
 				was_M = False
 				for state_M in states_M:
 					was_M = was_M or any([ state_M[s] == 0.5 for s in output_signals ])
 				
-				if times[i] <= cutoff_max and times[i+1] >= cutoff_min:
-					## region overlaps with cropped region
-					# t_from = max(cutoff_min, times[i])
-					# t_to =   min(cutoff_max, times[i+1])
-					t_from = max(cutoff_min, j)
-					t_to =   min(cutoff_max, j+0.1)
+				# if times[i] <= cutoff_max and times[i+1] >= cutoff_min:
+				# 	## region overlaps with cropped region
+				# 	t_from = max(cutoff_min, j)
+				# 	t_to =   min(cutoff_max, j+0.1)
 
-					if was_M:
-						susceptible_intervals += [ (s, [t_from, t_to]) ]
-						pos += t_to - t_from
-					else:
-						neg += t_to - t_from
+				if was_M:
+					susceptible_intervals += [ (s, [j, j+0.1]) ]
+					# pos += t_to - t_from
+					pos += 0.1
+				else:
+					# neg += t_to - t_from
+					neg += 0.1
 			
 				j = round(j + 0.1, 1)
+
+	for s in output_signals:
+		# pos_per_sig[s] += times[-1] - times[0]
+
+		if not exclude_output_signals:
+			susceptible_intervals += [ (s, [times[0], times[-1]]) ]
+			pos += times[-1] - times[0]
 
 
 	return {
--- libs/checkbi.py
+++ libs/checkbi.py
@@ -2,122 +2,269 @@ from libs import tracem as tr
 
 
 def getStateAtTime(times, states, time: float):
-    """
-    given the execution as <times> and <states>,
-    returns the state vector at time <time>
-    """
-    assert (time >= 0 and time <= times[-1]), "getStateAtTime: out of bounds time"
+	"""
+	given the execution as <times> and <states>,
+	returns the state vector at time <time>
+	"""
+	assert (time >= 0 and time <= times[-1]), "getStateAtTime: out of bounds time"
 
-    for i, t in enumerate(times):
-        if t > time:
-            return states[i-1]
+	for i, t in enumerate(times):
+		if t > time:
+			return states[i-1]
 
-    return states[-1]
+	return states[-1]
 
 
 def isSusceptible(t, s, states, s_state, events, T, output_signals, MafterGrid=0.001, Mdelta=0.001):
-    events_check = events + [
-        (t + MafterGrid,          s, 0.5),      # add glitch
-        (t + MafterGrid + Mdelta, s, s_state),  # reset glitch
-    ]
-    times_M, states_M = tr.trace(states[0], events=events_check, T=T, verbose=False)
-            
-    was_M = False
-    for state_M in states_M:
-        was_M = was_M or any([ state_M[s] == 0.5 for s in output_signals ])
+	events_check = events + [
+		(t + MafterGrid,          s, 0.5),      # add glitch
+		(t + MafterGrid + Mdelta, s, s_state),  # reset glitch
+	]
+	times_M, states_M = tr.trace(states[0], events=events_check, T=T, verbose=False)
+			
+	was_M = False
+	for state_M in states_M:
+		was_M = was_M or any([ state_M[s] == 0.5 for s in output_signals ])
 
-    return was_M
+	return was_M
 
 
 def findBoundary(tfrom, tto, s, states, s_state, events, T, output_signals, initially=True):
-    # print(tfrom, tto)
+	# print(tfrom, tto)
 
-    # check if anywhere marked
-    if not isSusceptible(t=tto, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=-0.05):
-        # no -> return boundary at end
-        return tto
+	# check if anywhere marked
+	if not isSusceptible(t=tto, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=-0.05):
+		# no -> return boundary at end
+		return tto
 
-    # check if all marked (only initially)
-    elif initially and isSusceptible(t=tfrom, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=0.001):
-        # yes -> return boundary at start
-        return tfrom
+	# check if all marked (only initially)
+	elif initially and isSusceptible(t=tfrom, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=0.001):
+		# yes -> return boundary at start
+		return tfrom
 
-    # else intersect
-    else:
-        middle = (tto + tfrom)/2
+	# else intersect
+	else:
+		middle = (tto + tfrom)/2
 
-        if tto - tfrom <= 0.05:
-            return middle
+		if tto - tfrom <= 0.05:
+			return middle
 
-        else:
-            middle_is_M = isSusceptible(t=middle, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=0.001)
+		else:
+			middle_is_M = isSusceptible(t=middle, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, MafterGrid=0.001)
 
-            if middle_is_M:
-                # boundary must be on the left half
-                return findBoundary(tfrom=tfrom, tto=middle, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, initially=False)
-            else:
-                # boundary must be on the right half
-                return findBoundary(tfrom=middle, tto=tto, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, initially=False)
+			if middle_is_M:
+				# boundary must be on the left half
+				return findBoundary(tfrom=tfrom, tto=middle, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, initially=False)
+			else:
+				# boundary must be on the right half
+				return findBoundary(tfrom=middle, tto=tto, s=s, states=states, s_state=s_state, events=events, T=T, output_signals=output_signals, initially=False)
 
 
 
 
 def check(times, states, events, signals, output_signals,
-    Textra=30,
-    exclude_output_signals=True,
-    cutoff_min=0, cutoff_max=float('Inf')):
-    """
-    checking all equivalence regions
-    """
-    susceptible_intervals = []
-    pos = 0
-    neg = 0
-    pos_per_sig = { s: 0 for s in signals }
-    T = times[-1] + Textra
+	Textra=30,
+	exclude_output_signals=True,
+	cutoff_min=0, cutoff_max=float('Inf')):
+	"""
+	checking all equivalence regions
+	"""
+	susceptible_intervals = []
+	pos = 0
+	neg = 0
+	pos_per_sig = { s: 0 for s in signals }
+	T = times[-1] + Textra
 
-    non_output_signals = [ s for s in signals if s not in output_signals ]
+	non_output_signals = [ s for s in signals if s not in output_signals ]
 
-    # go over regions
-    for s in non_output_signals:
-        for i in range(len(times)-1):
-            tfrom = times[i]
-            tto = times[i+1]
-            boundary = findBoundary(tfrom=tfrom, tto=tto, s=s, states=states, s_state=states[i][s], events=events, T=T, output_signals=output_signals)
+	# go over regions
+	for s in non_output_signals:
+		for i in range(len(times)-1):
+			tfrom = times[i]
+			tto = times[i+1]
+			boundary = findBoundary(tfrom=tfrom, tto=tto, s=s, states=states, s_state=states[i][s], events=events, T=T, output_signals=output_signals)
 
-            if boundary < tto:
-                # it has a marked area:
-                susceptible_intervals += [ (s, [boundary, tto]) ]
-            
-            assert (tto - boundary >= 0)
-            assert (boundary - tfrom >= 0)
+			if boundary < tto:
+				# it has a marked area:
+				susceptible_intervals += [ (s, [boundary, tto]) ]
+			
+			assert (tto - boundary >= 0)
+			assert (boundary - tfrom >= 0)
 
-            pos += tto - boundary
-            neg += boundary - tfrom
+			pos += tto - boundary
+			neg += boundary - tfrom
 
-            pos_per_sig[s] += tto - boundary
+			pos_per_sig[s] += tto - boundary
 
-    for s in output_signals:
-        pos_per_sig[s] += times[-1] - times[0]
+	for s in output_signals:
+		pos_per_sig[s] += times[-1] - times[0]
 
-        if not exclude_output_signals:
-            susceptible_intervals += [ (s, [times[0], times[-1]]) ]
-            pos += times[-1] - times[0]
+		if not exclude_output_signals:
+			susceptible_intervals += [ (s, [times[0], times[-1]]) ]
+			pos += times[-1] - times[0]
 
-            # if times[i] <= cutoff_max and times[i+1] >= cutoff_min:
-            #     # region overlaps with cropped region
-            #     t_from = max(cutoff_min, times[i])
-            #     t_to =   min(cutoff_max, times[i+1])
+			# if times[i] <= cutoff_max and times[i+1] >= cutoff_min:
+			#     # region overlaps with cropped region
+			#     t_from = max(cutoff_min, times[i])
+			#     t_to =   min(cutoff_max, times[i+1])
 
-            #     if was_M:
-            #         susceptible_intervals += [ (s, [t_from, t_to]) ]
-            #         pos += t_to - t_from
-            #     else:
-            #         neg += t_to - t_from
+			#     if was_M:
+			#         susceptible_intervals += [ (s, [t_from, t_to]) ]
+			#         pos += t_to - t_from
+			#     else:
+			#         neg += t_to - t_from
 
-    return {
-        'p': pos/(pos+neg) if pos + neg > 0 else 'undefined',
-        'p_per_sig': { s: pos_per_sig[s] / (times[-1] - times[0]) for s in signals },
-        'cutoff_min': cutoff_min,
-        'cutoff_max': cutoff_max,
-        'susceptible': susceptible_intervals
-    }
+	return {
+		'p': pos/(pos+neg) if pos + neg > 0 else 'undefined',
+		'p_per_sig': { s: pos_per_sig[s] / (times[-1] - times[0]) for s in signals },
+		'cutoff_min': cutoff_min,
+		'cutoff_max': cutoff_max,
+		'susceptible': susceptible_intervals
+	}
+
+
+
+
+def isSusceptibleSA(t, s, states, events, T, output_signals, SAF, MafterGrid=0.01,): #MafterGrid=0.001
+		
+	events_check = events + [
+		(t + MafterGrid, s, SAF),           # add SA0 or SA1
+	]
+	
+	times_M, states_M = tr.traceSA(states[0], events=events_check, SA_sig=s, SA_time=t+MafterGrid, T=T, verbose=False)
+	
+	was_M = False
+	for state_M in states_M:
+		was_M = was_M or any([ state_M[s] == 0.5 for s in output_signals ])
+		
+	return was_M
+
+
+def findBoundarySA(tfrom, tto, s, states, events, T, output_signals, SAF, start_is_M_initially, initially=True):
+	# print(tfrom, tto)
+
+	# global start_is_M_initially
+
+	# if initially:
+	# 	start_is_M_initially = isSusceptibleSA(t=tfrom, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, MafterGrid=0.001)
+
+	# check if anywhere marked
+	start_is_M = isSusceptibleSA(t=tfrom, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, MafterGrid=0.001)
+	end_is_M = isSusceptibleSA(t=tto, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, MafterGrid=-0.05)
+
+	if not start_is_M and not end_is_M:
+		# no -> return boundary at end
+		return tto
+
+	# check if all marked (only initially)
+	elif initially and start_is_M and end_is_M:
+		# yes -> return boundary at start
+		return tfrom
+
+	# else intersect
+	else:
+		middle = (tto + tfrom)/2
+
+		if tto - tfrom <= 0.05:
+			return middle
+
+		else:
+			middle_is_M = isSusceptibleSA(t=middle, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, MafterGrid=0.001)
+
+			if middle_is_M:
+				if not start_is_M_initially:
+					# boundary must be on the left half
+					return findBoundarySA(tfrom=tfrom, tto=middle, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, start_is_M_initially=start_is_M_initially, initially=False)
+				else:
+					# boundary must be on the right half
+					return findBoundarySA(tfrom=middle, tto=tto, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, start_is_M_initially=start_is_M_initially, initially=False)
+			else:
+				if start_is_M_initially:
+					# boundary must be on the left half
+					return findBoundarySA(tfrom=tfrom, tto=middle, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, start_is_M_initially=start_is_M_initially, initially=False)
+				else:
+					# boundary must be on the right half
+					return findBoundarySA(tfrom=middle, tto=tto, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, start_is_M_initially=start_is_M_initially, initially=False)
+
+
+def checkSA(times, states, events, signals, output_signals,
+	Textra=30,
+	exclude_output_signals=True,
+	cutoff_min=0, cutoff_max=float('Inf'),
+	fault='SA0'):
+	"""
+	checking all equivalence regions
+	"""
+	susceptible_intervals = []
+	pos = 0
+	neg = 0
+	pos_per_sig = { s: 0 for s in signals }
+	T = times[-1] + Textra
+	direction = 1
+
+	if fault == 'SA0':
+		SAF = 0
+	elif fault == 'SA1':
+		SAF = 1
+
+	non_output_signals = [ s for s in signals if s not in output_signals ]
+
+	# go over regions
+	for s in non_output_signals:
+		for i in range(len(times)-1):
+			tfrom = times[i]
+			tto = times[i+1]
+			start_is_M_initially = isSusceptibleSA(t=tfrom, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, MafterGrid=0.001)
+			boundary = findBoundarySA(tfrom=tfrom, tto=tto, s=s, states=states, events=events, T=T, output_signals=output_signals, SAF=SAF, start_is_M_initially=start_is_M_initially)
+
+			if boundary < tto:
+				# it has a marked area:
+				if boundary != tfrom and start_is_M_initially:
+				# if boundary != tfrom and start_is_M_initially:
+					susceptible_intervals += [ (s, [tfrom, boundary]) ]
+					direction = 0
+				else:
+					susceptible_intervals += [ (s, [boundary, tto]) ]
+			
+			assert (tto - boundary >= 0)
+			assert (boundary - tfrom >= 0)
+
+
+			if direction:
+				pos += tto - boundary
+				neg += boundary - tfrom
+			else:
+				## region goes backwards
+				pos += boundary - tfrom
+				neg += tto - boundary
+
+			# pos += tto - boundary
+			# neg += boundary - tfrom				
+
+			pos_per_sig[s] += pos
+
+	for s in output_signals:
+		pos_per_sig[s] += times[-1] - times[0]
+
+		if not exclude_output_signals:
+			susceptible_intervals += [ (s, [times[0], times[-1]]) ]
+			pos += times[-1] - times[0]
+
+			# if times[i] <= cutoff_max and times[i+1] >= cutoff_min:
+			#     # region overlaps with cropped region
+			#     t_from = max(cutoff_min, times[i])
+			#     t_to =   min(cutoff_max, times[i+1])
+
+			#     if was_M:
+			#         susceptible_intervals += [ (s, [t_from, t_to]) ]
+			#         pos += t_to - t_from
+			#     else:
+			#         neg += t_to - t_from
+
+	return {
+		'p': pos/(pos+neg) if pos + neg > 0 else 'undefined',
+		'p_per_sig': { s: pos_per_sig[s] / (times[-1] - times[0]) for s in signals },
+		'cutoff_min': cutoff_min,
+		'cutoff_max': cutoff_max,
+		'susceptible': susceptible_intervals
+	}
